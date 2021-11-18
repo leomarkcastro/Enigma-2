96ce3b2ef1fbc7ba0fc2a95800d80075bf0b1f2b
leomarkcastro <leomarkcastro123@gmail.com>
2021-11-18 13:57:06 +0800

    First Push

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  
+  if (req.method === 'POST') {
+    res.status(200).json({data: "Split Boi"})
+  } else {
+    res.status(200).send("Nothing to see here")
+  }
 }
