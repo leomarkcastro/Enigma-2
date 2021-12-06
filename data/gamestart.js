803ec4580e9221fef61dc7078215ffb332c0fdaa
leomarkcastro <leomarkcastro123@gmail.com>
2021-12-06 13:25:25 +0800

    added gmt on start date format to fix possible time misalignment

--- data/gamestart.js
+++ data/gamestart.js
@@ -1,5 +1,5 @@
 
-const start_date = "Dec 13, 2021 12:00:00"
+const start_date = "Dec 13 2021 12:00:00 GMT+0800"
 const check_start_date = function () { return (new Date(start_date).getTime() - new Date().getTime()) < 0 }
 
 export { check_start_date, start_date }
